3a820be1fc2e81a75454961fe5e81bac1129d1ce
EskandarAtrakchi <eskandaratrakchi8@gmail.com>
2025-11-25 10:16:14 +0000

    ports fixed

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/', indexRouter);
+
+// Basic error handler
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send('Something went wrong.');
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on http://localhost:${PORT}`);
+});
